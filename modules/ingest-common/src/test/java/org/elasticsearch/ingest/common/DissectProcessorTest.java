c96eb27d4c0d7605941001e8e0a19e28e71542a9
Jake Landis <jake.landis@elastic.co>
2018-07-16 18:33:48 -0500

    wip: processor

---
+++ modules/ingest-common/src/test/java/org/elasticsearch/ingest/common/DissectProcessorTest.java
@@ -0,0 +1,19 @@
+package org.elasticsearch.ingest.common;
+
+import org.elasticsearch.test.ESTestCase;
+
+public class DissectProcessorTest extends ESTestCase {
+
+    //TODO:
+//    public void testBasic() throws Exception {
+//
+//        IngestDocument document = new IngestDocument("_index", "_type", "_id", null, null, null,
+//            Collections.singletonMap("message", "foo,,baz"));
+//
+//
+////        DissectProcessor dissectProcessor = new DissectProcessor("", "message", "%{a},%{b},%{c}");
+////
+////        dissectProcessor.execute(document);
+//    }
+
+}
