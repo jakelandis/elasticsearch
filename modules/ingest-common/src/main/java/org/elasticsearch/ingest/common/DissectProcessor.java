1fd95d824ebfe4d6d01de11380305ab266edc26b
Jake Landis <jake.landis@elastic.co>
2018-08-01 16:31:41 -0500

    added some tests

--- modules/ingest-common/src/main/java/org/elasticsearch/ingest/common/DissectProcessor.java
+++ modules/ingest-common/src/main/java/org/elasticsearch/ingest/common/DissectProcessor.java
@@ -14,18 +14,42 @@ public final class DissectProcessor extends AbstractProcessor {
     public static final String TYPE = "dissect";
     private final String field;
     private final boolean ignoreMissing;
+    private final String pattern;
+    private final String appendSeparator;
     private final DissectParser dissectParser;
 
     DissectProcessor(String tag, String field, String pattern, String appendSeparator, boolean ignoreMissing) {
         super(tag);
         this.field = field;
         this.ignoreMissing = ignoreMissing;
+        this.pattern = pattern;
+        this.appendSeparator = appendSeparator;
         this.dissectParser = new DissectParser(pattern, appendSeparator);
+    }
+
+    String getField() {
+        return field;
+    }
+
+    boolean isIgnoreMissing() {
+        return ignoreMissing;
+    }
+
+    DissectParser getDissectParser() {
+        return dissectParser;
+    }
+
+    String getPattern() {
+        return pattern;
+    }
+
+    String getAppendSeparator() {
+        return appendSeparator;
     }
 
     @Override
     public void execute(IngestDocument ingestDocument) {
-        String input = ingestDocument.getFieldValue(field, String.class);
+        String input = ingestDocument.getFieldValue(field, String.class, ignoreMissing);
         if (input == null && ignoreMissing) {
             return;
         } else if (input == null) {
@@ -41,11 +65,11 @@ public final class DissectProcessor extends AbstractProcessor {
 
     public static final class Factory implements Processor.Factory {
 
-        public DissectProcessor create(Map<String, Processor.Factory> registry, String processorTag,
-                                       Map<String, Object> config) {
+        @Override
+        public DissectProcessor create(Map<String, Processor.Factory> registry, String processorTag, Map<String, Object> config) {
             String field = ConfigurationUtils.readStringProperty(TYPE, processorTag, config, "field");
             String pattern = ConfigurationUtils.readStringProperty(TYPE, processorTag, config, "pattern");
-            String appendSeparator = ConfigurationUtils.readOptionalStringProperty(TYPE, processorTag, config, "append_separator");
+            String appendSeparator = ConfigurationUtils.readStringProperty(TYPE, processorTag, config, "append_separator", "");
             boolean ignoreMissing = ConfigurationUtils.readBooleanProperty(TYPE, processorTag, config, "ignore_missing", false);
             return new DissectProcessor(processorTag, field, pattern, appendSeparator, ignoreMissing);
         }
