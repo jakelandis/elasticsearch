82252f4ee6bd2f1dc8d2e17aeacd7391f7c44ae2
Jake Landis <jake.landis@elastic.co>
2019-09-25 09:46:32 -0500

    introduce server-models for cluster state modeling

---
+++ server-models/src/main/java/org/elasticsearch/xcontent/GeneratedModels.java
@@ -0,0 +1,11 @@
+package org.elasticsearch.xcontent;
+
+import org.elasticsearch.common.xcontent.GeneratedModel;
+import org.elasticsearch.common.xcontent.ToXContent;
+
+@org.elasticsearch.common.xcontent.GeneratedModels({
+
+    @GeneratedModel("ilm/cluster_state_policy.json")
+})
+public interface GeneratedModels extends ToXContent {
+}
