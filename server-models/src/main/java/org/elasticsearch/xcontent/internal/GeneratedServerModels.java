52b0b352e5a7d6102eb0939e525f2fabfadcf90a
Jake Landis <jake.landis@elastic.co>
2019-09-25 10:28:49 -0500

    simplify the server model example..need to figure out how to re-use classes from spec-api-model

--- server-models/src/main/java/org/elasticsearch/xcontent/internal/GeneratedServerModels.java
+++ server-models/src/main/java/org/elasticsearch/xcontent/internal/GeneratedServerModels.java
@@ -5,7 +5,7 @@ import org.elasticsearch.common.xcontent.ToXContent;
 
 @org.elasticsearch.common.xcontent.GeneratedModels({
 
-    @GeneratedModel("ilm/cluster_state_policy.json")
+    @GeneratedModel("cluster_state.json")
 })
 public interface GeneratedServerModels extends ToXContent {
 }
