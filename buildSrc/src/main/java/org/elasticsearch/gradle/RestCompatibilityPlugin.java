31b8a872afc6edc0db8fa617ccf989e7ccd3ddf3
Jake Landis <jake.landis@elastic.co>
2020-06-24 17:46:16 -0500

    start building up the compat plugin for testing

--- buildSrc/src/main/java/org/elasticsearch/gradle/RestCompatibilityPlugin.java
+++ buildSrc/src/main/java/org/elasticsearch/gradle/RestCompatibilityPlugin.java
@@ -20,11 +20,16 @@
 package org.elasticsearch.gradle;
 
 import org.elasticsearch.gradle.info.BuildParams;
+import org.elasticsearch.gradle.test.RestIntegTestTask;
+import org.elasticsearch.gradle.test.rest.RestResourcesExtension;
+import org.elasticsearch.gradle.test.rest.YamlRestTestPlugin;
+import org.elasticsearch.gradle.testclusters.RestTestRunnerTask;
 import org.elasticsearch.gradle.util.GradleUtils;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.Dependency;
+import org.gradle.api.plugins.JavaBasePlugin;
 import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.SourceSetContainer;
 import org.gradle.plugins.ide.idea.model.IdeaModel;
@@ -33,16 +38,17 @@ import java.util.Map;
 
 public class RestCompatibilityPlugin implements Plugin<Project> {
 
-    private final String PREFIX = "v" + (VersionProperties.getElasticsearchVersion().getMajor() - 1);
-    private final String SOURCE_SET_NAME = PREFIX + "restCompatibility";
-    private final String TEST_SOURCE_SET_NAME = PREFIX + "restCompatibilityTest";
+    private final String COMPATIBLE_VERSION = "v" + (VersionProperties.getElasticsearchVersion().getMajor() - 1);
+    private final String CURRENT_VERSION = "v" + (VersionProperties.getElasticsearchVersion().getMajor());
+    private final String SOURCE_SET_NAME = COMPATIBLE_VERSION + "restCompatibility";
+    private final String TEST_SOURCE_SET_NAME = COMPATIBLE_VERSION + "restCompatibilityTest";
 
     @Override
     public void apply(Project project) {
-     //   project.getPluginManager().apply(ElasticsearchJavaPlugin.class);
+
 
         // create rest-compatibility source set
-        SourceSetContainer sourceSets = project.getExtensions().getByType(SourceSetContainer.class);
+        final SourceSetContainer sourceSets = project.getExtensions().getByType(SourceSetContainer.class);
         SourceSet restCompatSourceSet = sourceSets.create(SOURCE_SET_NAME);
 
         Configuration restCompatCompileConfig = project.getConfigurations().getByName(restCompatSourceSet.getCompileClasspathConfigurationName());
@@ -60,10 +66,10 @@ public class RestCompatibilityPlugin implements Plugin<Project> {
         GradleUtils.addTestSourceSet(project, TEST_SOURCE_SET_NAME);
         GradleUtils.extendSourceSet(project, SOURCE_SET_NAME, TEST_SOURCE_SET_NAME);
         SourceSet restCompatTestSourceSet = sourceSets.getByName(TEST_SOURCE_SET_NAME);
-        Configuration restCompatTestCompileConfig  = project.getConfigurations().getByName(restCompatTestSourceSet.getCompileClasspathConfigurationName());
+        Configuration restCompatTestCompileConfig = project.getConfigurations().getByName(restCompatTestSourceSet.getCompileClasspathConfigurationName());
         if (BuildParams.isInternal()) {
             Dependency testFrameworkDependency = project.getDependencies().project(Map.of("path", ":test:framework"));
-            Dependency restCompatTestDependency = project.getDependencies().project(Map.of("path", ":modules:rest-compatibility",  "configuration", "testArtifacts"));
+            Dependency restCompatTestDependency = project.getDependencies().project(Map.of("path", ":modules:rest-compatibility", "configuration", "testArtifacts"));
             project.getDependencies().add(restCompatTestCompileConfig.getName(), testFrameworkDependency);
             project.getDependencies().add(restCompatTestCompileConfig.getName(), restCompatDependency);
             project.getDependencies().add(restCompatTestCompileConfig.getName(), restCompatTestDependency);
@@ -77,24 +83,46 @@ public class RestCompatibilityPlugin implements Plugin<Project> {
         }
 
 
-//
-//
-//        //add the restCompatibility source set to the to the classpaths, don't override the existing classpath
-//        SourceSet restCompatTestSourceSet = sourceSets.getByName(TEST_SOURCE_SET_NAME);
-//        Configuration restCompatTestCompileConfig  = project.getConfigurations().getByName(restCompatTestSourceSet.getCompileClasspathConfigurationName());
-//        Configuration restCompatTestRuntimeConfig = project.getConfigurations().getByName(restCompatTestSourceSet.getRuntimeClasspathConfigurationName());
-//        restCompatTestSourceSet.setCompileClasspath(project.getObjects().fileCollection().from(restCompatSourceSet.getOutput(), restCompatTestCompileConfig));
-//        restCompatTestSourceSet.setRuntimeClasspath(project.getObjects().fileCollection().from(restCompatSourceSet.getOutput(), restCompatTestRuntimeConfig));
-////add dependency
-//        project.getDependencies().add(restCompatTestCompileConfig.getName(), compatModuleDependency);
-////
-
-
         project.getPluginManager().withPlugin("idea", p -> {
             IdeaModel idea = project.getExtensions().getByType(IdeaModel.class);
             idea.getModule().setSourceDirs(restCompatSourceSet.getJava().getSrcDirs());
 
         });
+
+
+        project.getPlugins().withType(YamlRestTestPlugin.class, plugin -> {
+            SourceSet yamlRestTestSourceSet = sourceSets.getByName(YamlRestTestPlugin.SOURCE_SET_NAME);
+            // create task - note can not use .register due to the work in RestIntegTestTask's constructor :(
+            // see: https://github.com/elastic/elasticsearch/issues/47804
+            RestIntegTestTask yamlRestTestTask = project.getTasks()
+                .create(
+                    YamlRestTestPlugin.SOURCE_SET_NAME + COMPATIBLE_VERSION,
+                    RestIntegTestTask.class,
+                    task -> {
+
+                        task.dependsOn(":distribution:bwc:minor:checkoutBwcBranch");
+                    }
+                );
+            yamlRestTestTask.setGroup(JavaBasePlugin.VERIFICATION_GROUP);
+            yamlRestTestTask.setDescription("Runs the " + COMPATIBLE_VERSION + " YAML based REST tests against an external "
+                + CURRENT_VERSION + "cluster with compatibility mode requested");
+
+            // setup the runner
+            RestTestRunnerTask runner = (RestTestRunnerTask) project.getTasks().getByName(yamlRestTestTask.getName() + "Runner");
+            runner.setTestClassesDirs(yamlRestTestSourceSet.getOutput().getClassesDirs());
+//            runner.setClasspath(project.getObjects().fileCollection().from(yamlRestTestSourceSet.getRuntimeClasspath(),
+//                restCompatTestSourceSet.getRuntimeClasspath()));
+            runner.setClasspath(project.getObjects().fileCollection().from(restCompatTestSourceSet.getRuntimeClasspath()));
+            runner.systemProperty("tests.rest.compat", Boolean.TRUE.toString());
+
+
+            //copy the 
+
+
+
+        });
+
+
 //        project.getPluginManager().withPlugin("eclipse", p -> {
 //            EclipseModel eclipse = project.getExtensions().getByType(EclipseModel.class);
 //            List<SourceSet> eclipseSourceSets = new ArrayList<>();
